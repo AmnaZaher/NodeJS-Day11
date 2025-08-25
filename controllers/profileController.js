c100f3750797f7569dca28d7fbb758b7dc029757
Amna <amnazizo70@gmail.com>
2025-08-25 16:27:38 +0300

    update

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,6 +1,5 @@
 const profile = (req, res) => {
-  const { name } = req.params;
-  return res.send(name);
+  res.render('profile', { title: 'Profile', name: req.params.name, age: req.params.age });
 };
 
 module.exports = {
